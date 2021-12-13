910353d47affbb929fdcaf7c858d77461faf005c
Emad ElFaramawi <emadf2k@gk.com>
2021-12-13 09:30:42 +0200

    Initial site creation

---
+++ index.php
@@ -0,0 +1,15 @@
+<html> 
+  <head> 
+    <title>Welcome To Skillsfot Global Knowledge</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <style>
+    body {font-family:"Verdana";}
+    h1   {font-family:"Verdana"; color:blue;}
+    </style>
+  </head>
+  <body> 
+    <a href="https://www.globalknowledge.com/"><img src="skillsoftGK.png" alt="SkillSoft Global Knowledge" /></a>
+    <h1>Welcome to Skillsoft Global Knowledge Version 1</h1>
+    Demo For GitHub DevOps Tools <a href="https://resources.github.com/devops/">here</a>.
+     </body> 
+</html>
